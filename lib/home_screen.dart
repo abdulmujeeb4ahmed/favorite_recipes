bca3d0677339ce36a59e2eea5e512531c4cf914c
abdulmujeeb4ahmed <abdulmujeeb4ahmed@gmail.com>
2025-02-21 20:20:58 -0500

    Made Update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,7 +17,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Updated recipe list with Indian dishes
     final List<Recipe> recipes = [
       Recipe(
         name: 'Butter Chicken',
